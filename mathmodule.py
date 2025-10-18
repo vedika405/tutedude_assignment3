04f0b23bb2559dee5a71e315fc5a87a1314c4a27
Vedika Chaturvedi <vedikachaturvedi89@gmail.com>
2025-10-19 02:58:39 +0530

    intial commit

---
+++ mathmodule.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+print(f"\nResults for the number {num}:")
+print(f"Square Root: {square_root}")
+print(f"Natural Logarithm (log base e): {natural_log}")
+print(f"Sine (in radians): {sine_value}")
